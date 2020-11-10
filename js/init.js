74a683062eb3a195ed22430b944ef3f0f4263d5b
Egor <rego@MacBook-Pro-Egor.local>
2020-11-10 14:47:32 +0300

    added

--- js/init.js
+++ js/init.js
@@ -86,87 +86,16 @@ jQuery(document).ready(function($) {
 
 
 /*----------------------------------------------------*/
-/* FitText Settings
------------------------------------------------------- */
-   setTimeout( function() {
 
-	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
-	   
-   }, 100);
 
 /*----------------------------------------------------*/
 /* Final Countdown Settings
 ------------------------------------------------------ */
-	var finalDate = '2016/01/01';
-
-	$('div#counter').countdown(finalDate)
-   	.on('update.countdown', function(event) {
-
-   		$(this).html(event.strftime('<span>%D <em>days</em></span>' + 
-   										 	 '<span>%H <em>hours</em></span>' + 
-   										 	 '<span>%M <em>minutes</em></span>' +
-   										 	 '<span>%S <em>seconds</em></span>'));
-
-   });  
 
 
 /*----------------------------------------------------*/
 /*	gmaps settings
 ------------------------------------------------------*/
-
-   var map;
-
-   // main directions
-   map = new GMaps({
-      el: '#map', 
-      lat: 14.549072, 
-      lng: 121.046958, 
-      zoom: 14, 
-      zoomControl : true,
-      zoomControlOpt: { style : 'SMALL', position: 'TOP_LEFT' }, 
-      panControl : false, 
-      scrollwheel: false
-   });
-
-   map.drawOverlay({
-	   lat: map.getCenter().lat(),
-	   lng: map.getCenter().lng(),
-	   content: '<i class="fa fa-map-marker"></i>',
-	   verticalAlign: 'top',
-	   horizontalAlign: 'center'
-	});
-
-   // The styles below present a simplified map. 
-   // If you would like to use a normal coloured map, then please remove or comment the code below, from lines 128 to 148.
-	var mapStyles = [
-	   {
-	      featureType: "road",
-	      elementType: "geometry",
-	      stylers: [{
-	         lightness: 100
-	      }, {
-	         visibility: "simplified"
-	      }]
-	   }, {
-	      featureType: "road",
-	      elementType: "labels",
-	      stylers: [{
-	         visibility: "off"
-	      }]
-	   }
-	];
-	
-	map.setOptions({
-	   styles: mapStyles
-	});
-
-   // map.addMarker({ 
- 		// lat: map.getCenter().lat(),
-	  //  lng: map.getCenter().lng(), 
- 		// title: '5th Avenue',
-   //  	infoWindow: { content: '<p>You can add your address 1 here</p>' } 
-   // });
-
 
 /*----------------------------------------------------*/
 /* Smooth Scrolling
